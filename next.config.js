8935e00128d29e2757e3a43bad20958b9ef46e10
Vinithra5 <vinithranaveen@gmail.com>
2025-03-06 17:08:04 -0500

    Configured GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: "/VinithraSadras_portfolio",  // Change this to match your repo name
+    images: {
+      unoptimized: true
+    }
+  };
+  
+  module.exports = nextConfig;
+  
